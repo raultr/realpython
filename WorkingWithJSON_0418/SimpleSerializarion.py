c4546e4c86fb144a29bf82acb934835f8714dcc4
RAUL ENRIQUE TORRES REYES <RAUL.TORRES@cusaem.gob.mx>
2018-09-17 17:52:51 -0500

    Generar el Json con identation

--- WorkingWithJSON_0418/SimpleSerializarion.py
+++ WorkingWithJSON_0418/SimpleSerializarion.py
@@ -9,7 +9,8 @@ def GuardarEstructuraEnArchivoJson():
     }
     with open("data_file.json", "w") as write_file:
         #Se le pasa el objeto a serializar y el archivo donde se guardara
-        json.dump(data, write_file)
+        #Tambien podemos especificarle si tendra identation
+        json.dump(data, write_file, indent=4)
 
 
 GuardarEstructuraEnArchivoJson()
