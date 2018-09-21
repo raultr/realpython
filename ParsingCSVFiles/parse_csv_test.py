4fd7cb5ff45ac3f045668e08022e94c0f28f5538
RAUL ENRIQUE TORRES REYES <RAUL.TORRES@cusaem.gob.mx>
2018-09-21 10:48:33 -0500

    Obtener indice de menor diferencia de goles

--- ParsingCSVFiles/parse_csv_test.py
+++ ParsingCSVFiles/parse_csv_test.py
@@ -6,6 +6,11 @@ class AnalizaCSVTest(unittest.TestCase):
 
     def setUp(self):
         self.archivo_csv = './csv/football.csv'
+        self.parsed_data = [
+            ['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals', 'Goals Allowed', 'Points'],
+            ['Arsenal', '38', '26', '9', '3', '79', '36', '87'],
+            ['Liverpool', '38', '24', '8', '6', '67', '30', '80']
+        ]
 
     def test_leer_cabeceras_del_archivo_csv(self):
         self.assertEqual(
@@ -20,23 +25,11 @@ class AnalizaCSVTest(unittest.TestCase):
         self.assertEqual(leer_archivo_csv(self.archivo_csv)[1][7], '87')
 
     def test_obtener_minima_diferencia_de_goles(self):
-        parsed_data = [
-            ['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals', 'Goals Allowed', 'Points'],
-            ['Arsenal', '38', '26', '9', '3', '79', '36', '87'],
-            ['Liverpool', '38', '24', '8', '6', '67', '30', '80']
-        ]
-
-        self.assertEqual(obtener_minima_diferencia_de_goles(parsed_data), 1)
+        self.assertEqual(obtener_minima_diferencia_de_goles(self.parsed_data), 1)
 
     def test_obtener_nombre_del_equipo(self):
-        parsed_data = [
-            ['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals', 'Goals Allowed', 'Points'],
-            ['Arsenal', '38', '26', '9', '3', '79', '36', '87'],
-            ['Liverpool', '38', '24', '8', '6', '67', '30', '80']
-        ]
-
-        indice = obtener_minima_diferencia_de_goles(parsed_data)
-        self.assertEqual(obtener_nombre(indice, parsed_data), 'Liverpool')
+        indice = obtener_minima_diferencia_de_goles(self.parsed_data)
+        self.assertEqual(obtener_nombre(indice, self.parsed_data), 'Liverpool')
 
 if __name__ == '__main__':
     unittest.main()
